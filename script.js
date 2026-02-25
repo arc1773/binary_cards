bff564e23579c375fd55f03ce40777af709fc960
arc1773 <123033839+arc1773@users.noreply.github.com>
2026-02-25 22:36:39 +0100

    0.0.2
    working version, styled

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
-var images = document.getElementsByClassName("chb");
+var images = document.getElementsByClassName("img");
 var states = new Array();
 
-var result = document.getElementById("result");
-var result_num = 0;
+var result_bin = document.getElementById("bin");
+var result_dec = document.getElementById("dec");
+var result_oct = document.getElementById("oct");
+var result_hex = document.getElementById("hex");
 
-setInterval(update, 10);
+var result_bin_num = 0
+var result_dec_num = 0
+var result_oct_num = 0
+var result_hex_num = 0
+
+
+images = Array.from(images).reverse();
+
+for (let i = 0; i < images.length; i++) {
+    images[i].addEventListener("click", ()=>{
+        states[i] = !states[i];
+        if(states[i]) images[i].src = "yes.png";
+        else images[i].src = "no.png";
+        update();
+    })
+    
+}
 function update(){
-    result_num = 0;
-    for (let i = 0; i < images.length; i++) {
-        states[i] = images[i].getElementById("myCheck").checked;
-    }
+    result_dec_num = 0
     for (let i = 0; i < states.length; i++) {
         if(states[i] == true){
-            result_num += Math.pow(2, i);
+            result_dec_num += Math.pow(2, i);
         }
             
     }
-
-    result.innerText = result_num;
+    result_bin.innerText = result_dec_num.toString(2);
+    result_dec.innerText = result_dec_num
+    result_oct.innerText = result_dec_num.toString(8);
+    result_hex.innerText = result_dec_num.toString(16);
 }
